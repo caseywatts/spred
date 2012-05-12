f2661efa666d93015019b19d4bc03b755dca7091
Casey Watts <casey.s.watts@gmail.com>
2012-05-12 13:47:03 -0400

    rails app new

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Spred::Application.config.secret_token = '08985a32afd2313701bd936293adeaf00a4025b562f089baf54087e685efc64dc48ed983585f633436195dee078aa917306f9a7551e626ef4a4b7ac6582f6073'
